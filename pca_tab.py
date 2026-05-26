28f28432a1d0a96df0bccf7f354b1aa098ada5e5
MarcelaBPVeiga <167220485+MarcelaBPV@users.noreply.github.com>
2026-05-26 15:36:49 -0300

    Update pca_tab.py

--- pca_tab.py
+++ pca_tab.py
@@ -340,9 +340,9 @@ def run_pca(df_raw):
 
             ax.text(
 
-                scores[i, 0] + 0.1,
+                scores[i, 0] + 0.12,
 
-                scores[i, 1] + 0.06,
+                scores[i, 1] + 0.08,
 
                 labels[i],
 
